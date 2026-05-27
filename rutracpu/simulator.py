be84605737d447f23bec2278c0287b3d79fc97d2
rutrarch <arturkummer08@gmail.com>
2026-05-27 18:03:00 +0200

    made it work on linux

--- rutracpu/simulator.py
+++ rutracpu/simulator.py
@@ -8,11 +8,20 @@ def run_command(command: list[str]) -> int:
 
 
 def run_simulation(mem_path: Path, gpu_frame_path: Path | None = None) -> int:
-    compile_rc = run_command(["iverilog", "-o", "rutracpu_tb", "hw/rutracpu.v", "hw/rutragpu.v", "hw/rutracpu_tb.v"])
+    compile_rc = run_command(
+        [
+            "iverilog",
+            "-o",
+            "rutracpu_tb",
+            "hw/rutracpu.v",
+            "hw/rutragpu.v",
+            "hw/rutracpu_tb.v",
+        ]
+    )
     if compile_rc != 0:
         return compile_rc
 
-    command = ["vvp", ".\\rutracpu_tb", f"+memfile={mem_path.as_posix()}"]
+    command = ["vvp", "./rutracpu_tb", f"+memfile={mem_path.as_posix()}"]
     if gpu_frame_path is not None:
         command.append(f"+gpufile={gpu_frame_path.as_posix()}")
     return run_command(command)
